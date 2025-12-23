8ed4ed34ca49adfa2ac3fe6bbd0a2ff22767cac3
Prathyuksha <iamprathi2006@gmail.com>
2025-12-23 18:13:36 +0530

    changed some files

--- admin_users.py
+++ admin_users.py
@@ -1,5 +1,14 @@
+import os
 import bcrypt
+from dotenv import load_dotenv
 
-password = os.getenv("password")   # 👈 nee use panna password
+# local-la mattum load aagum
+load_dotenv()
+
+password = os.getenv("ADMIN_PASSWORD")
+
+if not password:
+    raise Exception("ADMIN_PASSWORD not set")
+
 hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 print(hashed.decode())
